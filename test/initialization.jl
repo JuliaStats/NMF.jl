67815fa4891681b560f6008be737a5c2c738f252
Alex Arslan <ararslan@comcast.net>
2017-11-06 12:45:52 -0800

    Whoops

--- test/initialization.jl
+++ test/initialization.jl
@@ -30,7 +30,7 @@ W, H = NMF.nndsvd(X, 5)
 @test size(H) == (5, 12)
 @test all(W .>= 0.0)
 @test all(H .>= 0.0)
-@test vec(sum(abs2, W), 1) ≈ ones(5) atol=1.0e-8
+@test vec(sum(abs2, W, 1)) ≈ ones(5) atol=1.0e-8
 
 W2, H2 = NMF.nndsvd(X, 5; zeroh=true)
 @test size(W) == (8, 5)
