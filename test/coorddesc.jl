1fdcd9d05fc88f27aa6e8e824a02ec15ea0eb0ab
Unknown <vilim.stih@gmail.com>
2018-03-02 15:52:38 +0100

    Fixed a typing mistake in test

--- test/coorddesc.jl
+++ test/coorddesc.jl
@@ -9,7 +9,7 @@ for T in (Float64, Float32)
     Wg = max.(rand(T, p, k) .- T(0.5), zero(T))
     Hg = max.(rand(T, k, n) .- T(0.5), zero(T))
     X = Wg * Hg
-    W = Wg .+ rand(T, p, k)*0.1
+    W = Wg .+ rand(T, p, k)*T(0.1)
 
     NMF.solve!(NMF.CoordinateDescent{T}(α=0.0, maxiter=1000, tol=1e-9), X, W, Hg)
 
