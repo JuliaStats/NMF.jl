0a0952eabc270c6f9b1f6140a374708453de0122
Takehiro Sano <sano@momo.cs.okayama-u.ac.jp>
2021-02-14 07:28:36 +0900

    Fix bugs in examples/densenmf.jl

--- examples/densenmf.jl
+++ examples/densenmf.jl
@@ -1,16 +1,19 @@
 # NMF for dense matrices
 
 using NMF
+using Printf
+using Random
 
 function run(algname)
 
     # prepare data
+    Random.seed!(5678)
     p = 8
     k = 5
     n = 100
 
-    Wg = abs(randn(p, k))
-    Hg = abs(randn(k, n))
+    Wg = abs.(randn(p, k))
+    Hg = abs.(randn(k, n))
     X = Wg * Hg + 0.1 * randn(p, n)
 
     # run NNMF
@@ -19,7 +22,7 @@ function run(algname)
 
     r = nnmf(X, k; 
              init=:nndsvdar,
-             alg=symbol(algname), 
+             alg=Symbol(algname), 
              maxiter=30, 
              verbose=true)
 
