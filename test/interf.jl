JuliaStats/NMF.jl

commit 6e29f4d69d2d5a3c41466f7ad7a1adaadbf9ffa7

New stop condition: use component relative change (#83)
Previously, convergence was achieved when
`|W[i, j]-preW[i, j]|<=tol for all i, j`.
Running NMF on `10*X`  (where `X` is the data matrix )
would increase the number of iterations compared to NMF on `X`.
Now, `||W[:, j]-preW[:, j]||/||W[:, j]+preW[:, j]||<=tol for all j`
is the new convergence criterion, which is relative difference
rather absolute difference and thus, it is scale invariant.

Co-authored-by: youdongguo <1010705897@qq.com>

src/common.jl
@@ -70,10 +70,7 @@ function nmf_skeleton!(updater::NMFUpdater{T},
         update_wh!(updater, state, X, W, H)
 
         # determine convergence
-        dev = max(maxad(preW, W), maxad(preH, H))
-        if dev < tol
-            converged = true
-        end
+        converged = stop_condition(W, preW, H, preH, tol)
 
         # display info
         if verbose
@@ -89,4 +86,24 @@ function nmf_skeleton!(updater::NMFUpdater{T},
         objv = evaluate_objv(updater, state, X, W, H)
     end
     return Result{T}(W, H, t, converged, objv)
+end
+
+
+function stop_condition(W::AbstractArray{T}, preW::AbstractArray, H::AbstractArray, preH::AbstractArray, eps::AbstractFloat) where T
+    for j in axes(W,2)
+        dev_w = sum_w = zero(T)
+        for i in axes(W,1)
+            dev_w += (W[i,j] - preW[i,j])^2
+            sum_w += (W[i,j] + preW[i,j])^2
+        end
+        dev_h = sum_h = zero(T)
+        for i in axes(H,2)
+            dev_h += (H[j,i] - preH[j,i])^2
+            sum_h += (H[j,i] + preH[j,i])^2
+        end  
+        if sqrt(dev_w) > eps*sqrt(sum_w) || sqrt(dev_h) > eps*sqrt(sum_h)
+            return false
+        end 
+    end
+    return true    
 end

commit 64d5ca963308ba9d7c6fcdf15817242e42e7a2bc

Fix verbose printing
The `verbose=true` option was broken in #83, but it wasn't detected
because it was never tested.

src/common.jl
@@ -54,7 +54,7 @@ function nmf_skeleton!(updater::NMFUpdater{T},
     if verbose
         start = time()
         objv = evaluate_objv(updater, state, X, W, H)
-        @printf("%-5s    %-13s    %-13s    %-13s    %-13s\n", "Iter", "Elapsed time", "objv", "objv.change", "(W & H).change")
+        @printf("%-5s    %-13s    %-13s    %-13s    %-13s\n", "Iter", "Elapsed time", "objv", "objv.change", "(W & H).relchange")
         @printf("%5d    %13.6e    %13.6e\n", 0, 0.0, objv)
     end
 
@@ -70,7 +70,7 @@ function nmf_skeleton!(updater::NMFUpdater{T},
         update_wh!(updater, state, X, W, H)
 
         # determine convergence
-        converged = stop_condition(W, preW, H, preH, tol)
+        converged, dev = stop_condition(W, preW, H, preH, tol)
 
         # display info
         if verbose
@@ -90,6 +90,7 @@ end
 
 
 function stop_condition(W::AbstractArray{T}, preW::AbstractArray, H::AbstractArray, preH::AbstractArray, eps::AbstractFloat) where T
+    devmax = zero(T)
     for j in axes(W,2)
         dev_w = sum_w = zero(T)
         for i in axes(W,1)
@@ -100,10 +101,11 @@ function stop_condition(W::AbstractArray{T}, preW::AbstractArray, H::AbstractArr
         for i in axes(H,2)
             dev_h += (H[j,i] - preH[j,i])^2
             sum_h += (H[j,i] + preH[j,i])^2
-        end  
+        end
+        devmax = max(devmax, sqrt(max(dev_w/sum_w, dev_h/sum_h)))
         if sqrt(dev_w) > eps*sqrt(sum_w) || sqrt(dev_h) > eps*sqrt(sum_h)
-            return false
-        end 
+            return false, devmax
+        end
     end
-    return true    
+    return true, devmax
 end

test/interf.jl
@@ -35,5 +35,10 @@
             @test all(H .== ret.H)
             @test any(W .!= ret.W)
         end
+
+        # printing test
+        redirect_stdout(devnull) do
+            ret = NMF.nnmf(X, k, alg=:cd, init=:nndsvd, verbose=true)
+        end
     end
 end